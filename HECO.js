3ce9162d7e38189adaa99a72f7e8ef6272b6e564
awalghazal <awalghazal@gmail.com>
2024-10-01 23:46:02 +0300

    Update HECO.js

--- HECO.js
+++ HECO.js
@@ -8,7 +8,7 @@ let body = document.querySelector('body');
 let divHeader = document.createElement('div');
 body.appendChild(divHeader)
 let spanH = document.createElement('span');
-spanH.textContent = 'مستشفى الامير عون ياسر - قسم تنمية الإيرادات';
+spanH.textContent = 'مستشفى الامير عون ياسر الغزال - قسم تنمية الإيرادات';
 spanH.id = 'Hospital_Name';
 divHeader.appendChild(spanH)
 
