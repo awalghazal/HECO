328cdfa8e61ab1a8139ca0976f135db094821509
awalghazal <awalghazal@gmail.com>
2024-10-01 08:04:01 +0300

    Add files via upload

---
+++ HECO.js
@@ -0,0 +1,8 @@
+let body = document.querySelector('body');
+let divHeader = document.createElement('div');
+body.appendChild(divHeader)
+let spanH = document.createElement('span');
+spanH.textContent = 'مستشفى الامير سعود بن جلوي - قسم تنمية الإيرادات';
+spanH.id = 'Hospital_Name';
+divHeader.appendChild(spanH)
+
